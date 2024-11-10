0e62c69a525f742706c7c6a222012656ad863d0b
2-3-5-41 <93025618+2-3-5-41@users.noreply.github.com>
2024-11-09 20:21:11 -0500

    rewrite into usable boilerplate example

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # godot_tokio
 
-A very simple crate to make use of the Tokio runtime in Godot 4.1 using [godot-rust/gdext](https://github.com/godot-rust/gdext).
+This was made to prevent re-typing out the boilerplate for creating a tokio runtime godot object.
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,36 @@
-pub mod runtime;
+use godot::prelude::*;
+use tokio::runtime::{self, Runtime};
 
-#[cfg(feature = "tests")]
-mod test_nodes;
+#[derive(GodotClass)]
+#[class(base=Object)]
+pub struct AsyncRuntime {
+    base: Base<Object>,
+    runtime: Runtime,
+}
+
+#[godot_api]
+impl IObject for AsyncRuntime {
+    fn init(base: Base<Object>) -> Self {
+        #[cfg(feature = "single-thread")]
+        let runtime = runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+
+        #[cfg(feature = "multi-thread")]
+        let runtime = runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+
+        Self { base, runtime }
+    }
+}
+
+#[godot_api]
+impl AsyncRuntime {
+    pub const SINGLETON: &'static str = "Tokio";
+    pub fn runtime(&self) -> &Runtime {
+        &self.runtime
+    }
+}
