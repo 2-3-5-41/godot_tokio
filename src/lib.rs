a3c45a23ad8f9828364b844814adc5cec7c7a6ef
2-3-5-41 <93025618+2-3-5-41@users.noreply.github.com>
2025-01-31 22:53:49 -0500

    Forgot something important.

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,37 @@ use tokio::{
 struct GodotTokio;
 
 #[gdextension]
-unsafe impl ExtensionLibrary for GodotTokio {}
+unsafe impl ExtensionLibrary for GodotTokio {
+    fn on_level_init(level: InitLevel) {
+        match level {
+            InitLevel::Scene => {
+                let mut engine = Engine::singleton();
+
+                engine.register_singleton(AsyncRuntime::SINGLETON, &AsyncRuntime::new_alloc());
+            }
+            _ => (),
+        }
+    }
+
+    fn on_level_deinit(level: InitLevel) {
+        match level {
+            InitLevel::Scene => {
+                let mut engine = Engine::singleton();
+
+                if let Some(async_singleton) = engine.get_singleton(AsyncRuntime::SINGLETON) {
+                    engine.unregister_singleton(AsyncRuntime::SINGLETON);
+                    async_singleton.free();
+                } else {
+                    godot_warn!(
+                        "Failed to find & free singleton -> {}",
+                        AsyncRuntime::SINGLETON
+                    )
+                }
+            }
+            _ => (),
+        }
+    }
+}
 
 #[derive(GodotClass)]
 #[class(base=Object)]
